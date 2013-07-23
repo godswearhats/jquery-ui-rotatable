c9ac83db61780094ef34fb0325501ebff82fba9b
godswearhats <github@godswearhats.com>
2013-07-22 17:12:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+jquery-ui-rotatable is a plugin for jQuery UI that works in a similar way to Draggable and Resizable, without being as full-featured (please fork and send me pull requests!). By default, it puts a small rotation icon in the bottom left of whatever element you want to make rotatable. I chose that area because it was originally being used in [Herald](/godswearhats/herald) and the elements in that project were also resizable and I didn't want to interfere with the bottom/right controls for that.
+
+### Usage
+
+Somewhere in your HTML ...
+
+	<!-- prerequisites -->
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	
+	<script src="jquery.ui.rotatable.js"></script>
+	<link rel="stylesheet" href="jquery.ui.rotatable.css"><!-- this is small and will allow you to override look/feel of handle -->
+	
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#target').rotatable();
+		}
+	</script>
+	
+	<div id="target">Rotate me!</div>
+	
+Note that you should probably define a height and width for anything that you make rotatable, as the rotation happens around the center point of the element, and when you don't define these things, it could look and feel a little a strange.
+
+### Shortcomings
+
+This has not been well tested or well documented. My hope is that other people will use it and then either submit patches or ask for fixes. Maybe I'll have enough time to try to get it merged with the actual jQuery UI, but I'm not sure I'd have the bandwidth to do this on my own.
+
+### License
+
+Released under the [MIT license](http://jquery.org/license), like jQuery. 
---
+++ jquery.ui.rotatable.js
@@ -0,0 +1,90 @@
+(function( $, undefined ) {
+		
+$.widget("ui.rotatable", $.ui.mouse, {
+	
+	options: {
+		handle: false
+	},
+	
+	handle: function(handle) {
+		if (handle === undefined) {
+			return this.options.handle;
+		}
+		this.options.handle = handle;
+	},
+	
+	_create: function() {
+		var handle = $(document.createElement('div'));
+		handle.addClass('ui-rotatable-handle');
+		handle.draggable({ helper: 'clone', start: dragStart });
+		handle.on('mousedown', startRotate);
+		handle.appendTo(this.element);
+		this.options.handle = handle;
+		this.element.data('angle', 0);
+	}
+});
+
+var elementBeingRotated, mouseStartAngle, elementStartAngle;
+$(document).on('mouseup', stopRotate);
+
+function getElementCenter(el) {
+	var elementOffset = getElementOffset(el);
+	var elementCentreX = elementOffset.left + el.width() / 2;
+	var elementCentreY = elementOffset.top + el.height() / 2;
+	return Array(elementCentreX, elementCentreY);
+};
+
+function getElementOffset(el) {
+	performRotation(el, 0);
+	var offset = el.offset();
+	performRotation(el, el.data('angle'));
+	return offset;
+};
+
+function performRotation(el, angle) {
+	el.css('transform','rotate(' + angle + 'rad)');
+	el.css('-moz-transform','rotate(' + angle + 'rad)');
+	el.css('-webkit-transform','rotate(' + angle + 'rad)');
+	el.css('-o-transform','rotate(' + angle + 'rad)');
+};
+
+function dragStart(event) {
+	if (elementBeingRotated) return false;
+};
+
+function rotateElement(event) {
+	if (!elementBeingRotated) return false;
+
+	var center = getElementCenter(elementBeingRotated);
+	var xFromCenter = event.pageX - center[0];
+	var yFromCenter = event.pageY - center[1];
+	var mouseAngle = Math.atan2(yFromCenter, xFromCenter);
+	var rotateAngle = mouseAngle - mouseStartAngle + elementStartAngle;
+	
+	performRotation(elementBeingRotated, rotateAngle);
+	elementBeingRotated.data('angle', rotateAngle);
+	
+	return false;
+};
+
+function startRotate(event) {
+	elementBeingRotated = $(this).parent(); 
+	var center = getElementCenter(elementBeingRotated);
+	var startXFromCenter = event.pageX - center[0];
+	var startYFromCenter = event.pageY - center[1];
+	mouseStartAngle = Math.atan2(startYFromCenter, startXFromCenter);
+	elementStartAngle = elementBeingRotated.data('angle');
+
+	$(document).on('mousemove', rotateElement);
+	
+	return false;
+};
+
+function stopRotate(event) {
+	if (!elementBeingRotated) return;
+	$(document).unbind('mousemove');
+	setTimeout( function() { elementBeingRotated = false; }, 10 );
+	return false;
+};
+
+})(jQuery);
