834efe68dc62b051f67cd5e2a4aa4d1c762d3323
Frederic Ye <Aqua-Ye@users.noreply.github.com>
2016-05-31 19:19:41 +0200

    Make lib compatible with AMD

--- jquery.ui.rotatable.js
+++ jquery.ui.rotatable.js
@@ -1,7 +1,17 @@
-(function( $, undefined ) {
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "jquery" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery );
+	}
+}(function( $, undefined ) {
 
 $.widget("ui.rotatable", $.ui.mouse, {
-	widgetEventPrefix: "rotate",
+    widgetEventPrefix: "rotate",
 
     options: {
         handle: false,
@@ -262,4 +272,6 @@ $.widget("ui.rotatable", $.ui.mouse, {
 
 });
 
-})(jQuery);
+return $.ui.rotatable;
+
+}));
